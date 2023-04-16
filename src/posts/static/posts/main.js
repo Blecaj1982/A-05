05acc771f7154bc2495d8d4a0b0b88d0f5fa1f7c
Blecaj1982 <90987495+Blecaj1982@users.noreply.github.com>
2023-04-16 13:28:46 -0400

    completed up to loading more posts with a button click

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,5 +1,55 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
-helloWorldBox.innerHTML = 'hello <b>world</b>'
+
+$.ajax({
+    type: 'GET', 
+    utrl: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error) {
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.array.forEach(el => {
+                postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+            });
+        }, 100)
+
+    },
+    error: function(error) {
+        console.log(error)
+    }
+})
