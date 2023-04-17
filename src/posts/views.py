c98200cf100de28b3a759b2bf1cc293ebf0ac19b
Blecaj1982 <90987495+Blecaj1982@users.noreply.github.com>
2023-04-16 22:42:24 -0400

    completed upto adding a form to the modal

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
 
 # Create your views here.
 
 
 def post_list_and_create(request):
-    qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs})
+    form = PostForm(request.POST or None)
+    # qs = Post.objects.all()
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
     visible = 3
@@ -27,7 +40,20 @@ def load_post_data_view(request, num_posts):
             'author': obj.author.user.username
         }
         data.append(item)
-    return JsonResponse({'data':data[lower:upper], 'size': size})
+    return JsonResponse({'data':data[lower:upper], 'size':size})
+
+def like_unlike_post(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
+
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
